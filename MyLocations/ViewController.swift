2dd85b6e0f8a5b994939b04399fc19bb91e2a42e
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-02-03 17:55:39 -0500

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Tyler Bischoff on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
