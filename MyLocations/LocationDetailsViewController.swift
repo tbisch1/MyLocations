7f93842a9ed87929067dc3eb585b165a66c92a52
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-02-11 03:58:45 -0500

    File moved inside project files

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreLocation
+import CoreData
 class LocationDetailsViewController: UITableViewController {
     @IBOutlet var descriptionTextView: UITextView!
     @IBOutlet var categoryLabel: UILabel!
@@ -14,6 +15,8 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet var longitudeLabel: UILabel!
     @IBOutlet var addressLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    var managedObjectContext: NSManagedObjectContext!
+    var date = Date()
     
     @IBAction func categoryPickerDidPickCategory(_ segue: UIStoryboardSegue) {
         let controller = segue.source as! CategoryPickerViewController
@@ -44,7 +47,7 @@ class LocationDetailsViewController: UITableViewController {
         else {
             addressLabel.text = "No Address Found"
         }
-        dateLabel.text = format(date: Date())
+        dateLabel.text = format(date: date)
         
         // Hide keyboard
        let gestureRecognizer = UITapGestureRecognizer(
@@ -70,11 +73,27 @@ class LocationDetailsViewController: UITableViewController {
         let hudView = HudView.hud(inView: mainView, animated: true)
         hudView.text = "Tagged"
         
-        afterDelay(0.6) {
-            hudView.hide()
-            self.navigationController?.popViewController(animated: true)
+        let location = Location(context: managedObjectContext)
+          
+        location.locationDescription = descriptionTextView.text
+        location.category = categoryName
+        location.latitude = coordinate.latitude
+        location.longitude = coordinate.longitude
+        location.date = date
+        location.placemark = placemark
+          
+        do {
+            try managedObjectContext.save()
+            afterDelay(0.6) {
+                hudView.hide()
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+        catch {
+            fatalCoreDataError(error)
         }
     }
+    
     @IBAction func cancel() {
     navigationController?.popViewController(animated: true)
     }
